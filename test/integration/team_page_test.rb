cdbedd212fa5ae71b1b7fb27fa73725e208fcf51
Yves Senn <yves.senn@gmail.com>
2013-02-09 20:12:40 +0100

    example usage of the new team structure

---
+++ test/integration/team_page_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class HomePageTest < ActionDispatch::IntegrationTest
+
+  test "the home displays the ruvetia organizers" do
+    visit '/team'
+    members = page.all('.team li')
+    assert_equal ["Yves Senn", "Rodrigo Haenggi", "Daniel Puglisi", "Kaspar Schiess", "Ferdinand Niedermann"], members.map(&:text)
+  end
+
+end
