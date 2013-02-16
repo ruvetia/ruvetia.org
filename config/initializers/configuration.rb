828d2ecf036e8d5e5a72163eb54ac08a2b8da216
Yves Senn <yves.senn@gmail.com>
2013-02-16 16:27:53 +0100

    display links to social media

---
+++ config/initializers/configuration.rb
@@ -0,0 +1,6 @@
+$ruvetia = {
+  mailing_list: 'https://groups.google.com/forum/?fromgroups#!forum/ruvetia',
+  email: 'ruvetia@gmail.com',
+  twitter: 'https://twitter.com/ruvetia',
+  github: 'https://github.com/ruvetia'
+}
