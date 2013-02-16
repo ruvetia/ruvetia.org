7d382aa5a669d4485a3fa69f7638c47e4f2d554d
Yves Senn <yves.senn@gmail.com>
2013-02-16 18:14:08 +0100

    updated team setup

--- config/initializers/team_setup.rb
+++ config/initializers/team_setup.rb
@@ -2,26 +2,26 @@
 $team = Team.new
 # ruvetia organizers in no particular order
 $team << Organizer.new('Yves Senn',
-                       social_media: { github: 'senny', twitter: 'fistofsenn' },
+                       social_media: { github: 'http://github.com/senny', twitter: 'https://twitter.com/fistofsenn' },
                        gravatar_hash: "3d698e2872c07061a455d9e250861235",
-                       bio: "")
+                       bio: "Yves started experimenting with ruby in late 2007. Since then he worked on medium to large scale Rails applications with a strong focus on automated testing. He is actively contributing to Rails and other Open Source projects like spring and queue_classic.")
 
 $team << Organizer.new('Rodrigo Haenggi',
-                       social_media: { github: 'therod', twitter: 'therod' },
+                       social_media: { github: 'http://github.com/therod', twitter: 'https://twitter.com/therod' },
                        gravatar_hash: "961ce8457830cbe5607fd365f134710c",
                        bio: "")
 
 $team << Organizer.new('Daniel Puglisi',
-                       social_media: { github: 'danielpuglisi', twitter: 'danielpuglisi' },
+                       social_media: { github: 'http://github.com/danielpuglisi', twitter: 'https://twitter.com/danielpuglisi' },
                        gravatar_hash: "34fd3386654f1b625e1d3b8b6c43a217",
-                       bio: "")
+                       bio: "Ruby, Ruby, Ruby...")
 
 $team << Organizer.new('Kaspar Schiess',
-                       social_media: { github: 'kschiess', twitter: 'kasparschiess' },
+                       social_media: { github: 'http://github.com/kschiess', twitter: 'https://twitter.com/kasparschiess' },
                        gravatar_hash: "ae4df18bf57748120b915d63b7e233d4",
                        bio: "")
 
 $team << Organizer.new('Ferdinand Niedermann',
-                       social_media: { github: 'nerdinand', twitter: 'nerdinand' },
+                       social_media: { github: 'http://github.com/nerdinand', twitter: 'https://twitter.com/nerdinand' },
                        gravatar_hash: "a69ea888ca7c5102e04e59a54d7db420",
-                       bio: "")
+                       bio: "Ferdinand got started with Ruby and Rails during his time at the University of Basel. He was soon amazed by the helpful and very active community around Ruby and worked at different companies to apply his Rails knowledge.")
