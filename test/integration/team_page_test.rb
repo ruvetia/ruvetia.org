b22d167a8b7cb5b4895288b75f7bb9c6562176d2
Yves Senn <yves.senn@gmail.com>
2013-02-16 18:30:05 +0100

    fix the build :fire:

--- test/integration/team_page_test.rb
+++ test/integration/team_page_test.rb
@@ -4,7 +4,7 @@ class HomePageTest < ActionDispatch::IntegrationTest
 
   test "the home displays the ruvetia organizers" do
     visit '/team'
-    members = page.all('.team li')
+    members = page.all('.team li h3')
     assert_equal ["Yves Senn", "Rodrigo Haenggi", "Daniel Puglisi", "Kaspar Schiess", "Ferdinand Niedermann"], members.map(&:text)
   end
 
