6c8dfb70aa87d7da0379820ee078a379899fb0cf
Yves Senn <yves.senn@gmail.com>
2013-02-17 15:28:06 +0100

    we need to keep the build :green_heart:

--- test/integration/team_page_test.rb
+++ test/integration/team_page_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class HomePageTest < ActionDispatch::IntegrationTest
 
   test "the home displays the ruvetia organizers" do
-    visit '/team'
+    visit '/'
     members = page.all('.team li h3')
     assert_equal ["Yves Senn", "Rodrigo Haenggi", "Daniel Puglisi", "Kaspar Schiess", "Ferdinand Niedermann"], members.map(&:text)
   end
