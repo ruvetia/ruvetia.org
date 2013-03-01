2c832a59bacb8112493ee9c594ce0b91d3bedb0c
Daniel Puglisi <pulleasy@gmail.com>
2013-03-01 20:37:25 +0100

    responsive as fuck

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
   def map(src)
-    raw "<iframe width='100%' height='700' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='#{src}&amp;output=embed&amp;iwloc=near'></iframe>"
+    raw "<iframe class='map' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='#{src}&amp;output=embed&amp;iwloc=near'></iframe>"
   end
 
   def ribbon(text, options={})
