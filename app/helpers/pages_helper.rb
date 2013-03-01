3784ab61b773245cf99eba6ed8e9318ac5465923
Daniel Puglisi <pulleasy@gmail.com>
2013-03-01 19:41:18 +0100

    added fucking ribbons - everybody loves ribbons

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,19 @@
 module PagesHelper
   def map(src)
-    raw "<iframe width='100%' height='500' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='#{src}&amp;output=embed&amp;iwloc=near'></iframe>"
+    raw "<iframe width='100%' height='700' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='#{src}&amp;output=embed&amp;iwloc=near'></iframe>"
+  end
+
+  def ribbon(text, options={})
+    content_tag :div, class: "fancy-divider " do
+      content_tag :div, class: "ribbon-wrapper ribbon-#{options[:class]}" do
+        content_tag(:div, text, class: "ribbon-front") +
+        content_tag(:div, "",   class: "ribbon-edge-topleft") +
+        content_tag(:div, "",   class: "ribbon-edge-topright") +
+        content_tag(:div, "",   class: "ribbon-edge-bottomleft") +
+        content_tag(:div, "",   class: "ribbon-edge-bottomright") +
+        content_tag(:div, "",   class: "ribbon-back-left") +
+        content_tag(:div, "",   class: "ribbon-back-right")
+      end
+    end
   end
 end
