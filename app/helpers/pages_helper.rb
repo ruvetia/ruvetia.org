49446010fcbe2791c375b1390b2d4b2d8cf44e28
Daniel Puglisi <pulleasy@gmail.com>
2013-03-01 18:00:25 +0100

    added different section styles

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,5 @@
 module PagesHelper
   def map(src)
-    raw "<iframe width='450' height='450' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='#{src}&amp;output=embed&amp;iwloc=near'></iframe>"
+    raw "<iframe width='100%' height='500' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='#{src}&amp;output=embed&amp;iwloc=near'></iframe>"
   end
 end
