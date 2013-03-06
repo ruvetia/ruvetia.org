ec3a1cf7ebff4e77e5604ec916b0a03d042e6656
Yves Senn <yves.senn@gmail.com>
2013-03-06 11:52:23 +0100

    add location fpr Ruvetia #1

--- config/initializers/meetup_setup.rb
+++ config/initializers/meetup_setup.rb
@@ -2,7 +2,8 @@ $meetups = [
             Meetup.new(
                        number: 1,
                        city: "Bern",
+                       location: "Musigbistrot",
                        time: "15.03.2013 18:00",
-                       map_src: "https://maps.google.com/maps?q=maps+bern&hl=en&hnear=Berne,+Canton+of+Bern,+Switzerland&t=m&z=12"
+                       map_src: "https://maps.google.ch/maps?q=musigbistrot+bern,+M%C3%BChlemattstrasse,+Bern&hl=de&ie=UTF8&ll=46.944769,7.433066&spn=0.009419,0.017209&sll=46.946161,7.432938&sspn=0.009419,0.017209&oq=musigbi&hq=musigbistrot+bern,+M%C3%BChlemattstrasse,+Bern&t=m&z=16"
                       )
            ]
