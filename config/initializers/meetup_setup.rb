69c11e5f3843702766e26abfb2d4ab14050eef33
Yves Senn <yves.senn@gmail.com>
2014-05-13 09:25:24 +0200

    publish upcomming Ruvetia #6 Meetup in Bern.

--- config/initializers/meetup_setup.rb
+++ config/initializers/meetup_setup.rb
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 $meetups = [
             Meetup.new(
+                       id: 6,
+                       number: 6,
+                       city: "Bern",
+                       location: "Musigbistrot",
+                       time: "13.06.2014 18:00",
+                       map_src: "https://maps.google.ch/maps?q=musigbistrot+bern,+M%C3%BChlemattstrasse,+Bern&hl=de&ie=UTF8&ll=46.944769,7.433066&spn=0.009419,0.017209&sll=46.946161,7.432938&sspn=0.009419,0.017209&oq=musigbi&hq=musigbistrot+bern,+M%C3%BChlemattstrasse,+Bern&t=m&z=16"
+                      ),
+
+            Meetup.new(
                     id: 5,
                     number: 4,
                     city: "Lausanne",
