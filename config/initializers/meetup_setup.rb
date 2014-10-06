692ece3fe0164f9d70a25654ad9d6ad7cef15e27
Felipe Kaufmann <felipekaufmann@gmail.com>
2014-10-06 14:57:22 +0200

    add location and map

--- config/initializers/meetup_setup.rb
+++ config/initializers/meetup_setup.rb
@@ -4,9 +4,9 @@ $meetups = [
                        id: 7,
                        number: 7,
                        city: "Luzern",
-                       location: "TBD",
+                       location: "Rathaus Brauerei",
                        time: "17.10.2014 18:00",
-                       map_src: ""
+                       map_src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2718.4057738592937!2d8.306318999999991!3d47.05188999999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47901f82f141019f%3A0x3dd4ed83fc6bcb61!2sRathaus+Brauerei!5e0!3m2!1sen!2sch!4v1412599978168"
                       ),
 
             Meetup.new(
