f1a037838c8ad592d431a8a0e43ee789e6455a61
Daniel Puglisi <pulleasy@gmail.com>
2015-04-24 16:02:35 +0200

    fix meetup test

--- test/integration/meetup_test.rb
+++ test/integration/meetup_test.rb
@@ -6,7 +6,7 @@ class MeetupPageTest < ActionDispatch::IntegrationTest
   test "the home displays the upcomming meetups" do
     visit '/'
     meetups = page.all('.meetups li h3').map(&:text)
-    assert_equal ['Ruvetia #7, Luzern'], meetups
+    assert_equal ['Ruvetia #8, Basel'], meetups
   end
 
   test "you can attend a meetup using github" do
